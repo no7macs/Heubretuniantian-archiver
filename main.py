cec78f2c4c15ce137b48183f04c67c1aa37cf746
no7mac <44577325+no7macs@users.noreply.github.com>
2021-02-07 17:15:05 -0700

    alomst done, just need add to get past messages and rebuild to upload past messages

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import json
 import os
 import requests
-import time
+from datetime import date
 import discord as discord
 
 client = discord.Client()
@@ -16,22 +16,34 @@ async def on_message(message):
     if message.author.bot == False:
         if message.content[0:2] == 'h ':
             content = message.content[2:]
+            #Adds new channel to archive
             if content.split(' ')[0] == 'add':
                 print('kek lmao')
-                if not 'channels' in loadedjsonsettings:
-                    loadedjsonsettings['channels'] = []
-                if not message.channel.id in loadedjsonsettings['channels']:
-                    loadedjsonsettings['channels'].append(message.channel.name)
+                if not os.path.isdir('./contents/' + message.channel.name):
                     os.mkdir('./content/' + message.channel.name)
-                with open('channels.json', 'w') as jsonFile:
-                    json.dump(loadedjsonsettings, jsonFile, indent=4)
-                    jsonFile.close()
+            #Rebuilds all files
+            elif content.split(' ')[0] == 'rebuild':
+                #Checks if category exists and if it doesn't creates it
+                for a in message.guild.categories:
+                    if a.name == 'Heubretuniantian archives': exists=True
+                    else: exists=False
+                if exists == False:
+                    await message.guild.create_category('Heubretuniantian archives')
+                #Puts all files in the category if it is the category
+                for a in message.guild.categories:
+                    print(a)
+                    if a.name == 'Heubretuniantian archives':
+                        for b in os.listdir('content'):
+                            await message.guild.create_text_channel(b, category=a, nsfw=True)
         elif message.author.id in [548571901916610560] and message.channel.name in loadedjsonsettings['channels']:
             print(message.attachments)
+            print(message.attachments[0].url)
             #saves file
-            fileDat = requests.get(message.attachments.url)
-            with open('./contents/' + message.channel.name + '/' + (strftime("%Y-%m-%d-%H-%M-%S", gmtime() if os.path.isfile(message.attachments.filename) else message.attachments.filename)), 'wb') as savedFile:
-                savedFile.write(fileDat.contents)
-                savedFile.close
+            for a in message.attachments:
+                fileDat = requests.get(a.url)
+                filename = './content/' + message.channel.name + '/' + (date.today().strftime("%Y-%m-%d-%H-%M-%S") if os.path.isfile(a.filename) else a.filename)
+                with open(filename, 'wb') as savedFile:
+                    savedFile.write(fileDat.content)
+                    savedFile.close
 
 client.run(open('./token.txt', 'r').read())
